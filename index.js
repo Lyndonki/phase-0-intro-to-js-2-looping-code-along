888cae33b3150c2c5e1257c251880f9d823bbc43
Xanderk <Xanderkidwell@gmail.com>
2022-06-20 21:34:22 -0700

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(name) {
+    const cards = []
+    let i = 0;
+    while (i < name.length) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        i++;
+    }
+    return cards;
+}
+function countDown(number) {
+    let i = 0
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+countDown(10);
